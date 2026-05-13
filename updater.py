c58039a1d96b4613b47275139e8bbcf7a38b26fc
Aluiel <heaven99902@gmail.com>
2026-05-14 02:12:05 +0300

    update version from 2.0.0 to 2.0.1

--- updater.py
+++ updater.py
@@ -20,7 +20,7 @@ except Exception:
     QVBoxLayout = None
 
 
-CURRENT_VERSION = "2.0.0"
+CURRENT_VERSION = "2.0.1"
 GITHUB_REPO = "ALuiell/BonkScanner"
 GITHUB_API_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
 
