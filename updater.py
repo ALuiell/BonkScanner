8265279918f812460d30c4fa378d50c94a71d663
Aluiel <heaven99902@gmail.com>
2026-05-23 09:02:42 +0300

    Bump version to 2.0.6

--- updater.py
+++ updater.py
@@ -21,7 +21,7 @@ except Exception:
     QVBoxLayout = None
 
 
-CURRENT_VERSION = "2.0.5"
+CURRENT_VERSION = "2.0.6"
 GITHUB_REPO = "ALuiell/BonkScanner"
 GITHUB_API_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
 
